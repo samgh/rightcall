f39e983485dafd53925ee04216455ce723dc1b31
Sam Gavis-Hughson <sgavis@princeton.edu>
2013-08-08 16:27:50 -0400

    adding login features :

--- main.py
+++ main.py
@@ -10,49 +10,12 @@ import jinja2
 import webapp2
 import cgi
 
+import invitations
+import users
+
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'])
-
-INVITATIONS_DB_NAME = 'invitations_db'
-
-def db_key(db_name=INVITATIONS_DB_NAME):
-    return ndb.Key('Insert', db_name)
-
-class InvitationRequest(ndb.Model):
-    email = ndb.StringProperty()
-    date = ndb.DateTimeProperty(auto_now_add=True)
-
-class Insert(webapp2.RequestHandler):
-    def post(self):
-        db_name = self.request.get('db_name', INVITATIONS_DB_NAME)
-
-        email = cgi.escape(self.request.get('email-signup')).strip()
-        redirect = self.request.get('redirect')
-        query = InvitationRequest.query(InvitationRequest.email == email)
-        inDB = query.fetch(1)
-        if not inDB:
-            invitationRequest = InvitationRequest(parent=db_key(db_name))
-            invitationRequest.email = cgi.escape(self.request.get('email-signup'))
-            invitationRequest.put()
-            self.redirect(redirect)
-        else:
-            self.redirect(redirect)
-
-
-class Response(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('<html><body>')
-        db_name = self.request.get('db_name', INVITATIONS_DB_NAME)
-
-        emails_query = InvitationRequest.query(ancestor=db_key(db_name)).order(-InvitationRequest.date)
-        emails = emails_query.fetch(100)
-
-        for message in emails:
-            self.response.write(cgi.escape(message.email))
-            self.response.write('&nbsp%s<br />' % message.date)
-
-        #self.redirect('/')
 
 class Home(webapp2.RequestHandler):
     def get(self):
@@ -102,8 +65,8 @@ application = webapp2.WSGIApplication([
     #('/privacy', Privacy),
     #('/about', About),
     ('/blog', Blog),
-    ('/insert', Insert),
-    #('/response', Response),
+    ('/insert', invitations.Insert),
+    ('/response', invitations.Response),
     ('/adwords.*', Adwords),
     ('/.*', NotFoundPageHandler)
 ], debug=True)
